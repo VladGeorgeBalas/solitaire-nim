2939c9767fd827d3ab92046cd6d49a29b9c1d882
Buwubie <152532979+Buwubie@users.noreply.github.com>
2024-04-11 13:31:06 +0100

    adaugiri explicative

--- card.nim
+++ card.nim
@@ -4,33 +4,39 @@ type card* = object
   faceUp : bool
   next : ref card
 
+#constructor
 proc newCard*() : ref card =
   return new card
 
+#iterate all items of the linked list
 iterator items*(self : ref card) : ref card =
   var tmp = self
   while tmp != nil :
     yield tmp
     tmp = tmp.next
 
+#number getter/setter
 proc number*(self : ref card) : char =
   return self.number
 
 proc `number=`*(self : ref card, val : char) =
   self.number = val
 
+#suit getter/setter
 proc suit*(self : ref card) : char =
   return self.suit
 
 proc `suit=`*(self : ref card, val : char) =
   self.suit = val
 
+#faceUp getter/setter
 proc faceUp*(self : ref card) : bool =
   return self.faceUp
 
 proc `faceUp=`*(self : ref card, val : bool) =
   self.faceUp = val
 
+#next getter/setter
 proc next*(self : ref card) : ref card=
   return self.next
 
--- deck.nim
+++ deck.nim
@@ -1,8 +1,15 @@
 import card
 
-type deck = object of RootObj
+type deck* = object of RootObj
   top : ref card
 
+#returns iterator for the list
 proc cards*(self : deck) : ref card =
   return self.top
 
+#top getter/setter
+proc top(self : deck) : ref card =
+  return self.top
+
+proc `top=`(self : var deck, val : ref card) =
+  self.top = val
